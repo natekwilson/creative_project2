104a4b481e3d6c721ab83f79dca6db8da4e70956
Nate Wilson <46329080+natekwilson@users.noreply.github.com>
2019-10-11 20:29:10 -0600

    Update artistAPI.js

--- js/artistAPI.js
+++ js/artistAPI.js
@@ -15,7 +15,7 @@ document.getElementById("artistSubmit").addEventListener("click", function(event
 		for (let i=0; i < relatedArtists.length ; i++) 
 		{
 			results += "<p>" + relatedArtists[i].name + "</p>" + "<br>";
-			results += "<img size= \'medium\'>" + relatedArtists[i].image[2].#text + "</img>" + "<br>";
+			results += "<img size= \'medium\'>" + relatedArtists[i].image[2]['#text'] + "</img>" + "<br>";
 		}
       document.getElementById("artistResults").innerHTML = results;
     });

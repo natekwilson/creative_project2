8c9313b97559cd709dc261b804b19c776d63f0a2
Nate Wilson <46329080+natekwilson@users.noreply.github.com>
2019-10-11 21:45:09 -0600

    Update artistAPI.js

--- js/artistAPI.js
+++ js/artistAPI.js
@@ -4,7 +4,7 @@ document.getElementById("artistSubmit").addEventListener("click", function(event
   if (value === "")
     return;
   console.log(value);
-    const url = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=6&artist=" + value + "&api_key=ed1e95133fb0f5ae801a5af676184bb0&format=json";
+    const url = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=10&artist=" + value + "&api_key=ed1e95133fb0f5ae801a5af676184bb0&format=json";
   fetch(url)
     .then(function(response) {
       return response.json();
